00c927c8f5821fe4088f526f7e67d31afae2ff09
Energy1190 <energyneo0@gmail.com>
2017-11-16 14:01:02 +0300

    Update remessage.sh

--- remessage.sh
+++ remessage.sh
@@ -3,7 +3,12 @@
 regexp_message () {
   echo "Hello, i'm regexp function"
   echo "My message is: $1"
-  echo "My regexp string is :$2"
+  echo "My regexp string is: $2"
+  if [[ $1 =~ $2]]: then
+  echo "All good"
+  else
+  echo "Not good"
+  fi
 }
 
 echo "Hello, i'm temp script."

627d1acfb30915ab2afaa8cceb92c4311fd4a999
Energy1190 <energyneo0@gmail.com>
2017-11-16 13:57:28 +0300

    Update remessage.sh

--- remessage.sh
+++ remessage.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-printenv
+regexp_message () {
+  echo "Hello, i'm regexp function"
+  echo "My message is: $1"
+  echo "My regexp string is :$2"
+}
 
 echo "Hello, i'm temp script."
 
 echo "My var GIT_PREVIOUS_SUCCESSFUL_COMMIT is: ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
-echo "My var GIT_PREVIOUS_SUCCESSFUL_COMMIT is: ${GIT_COMMIT}" 
-echo "My var GIT_PREVIOUS_SUCCESSFUL_COMMIT is: GIT LOG:"
+echo "My var GIT_COMMIT is: ${GIT_COMMIT}" 
+
 COMMIT_ARRAY=$(git rev-list ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}^..HEAD)
 for commit in ${COMMIT_ARRAY[@]}
 do
@@ -14,7 +18,7 @@ if [ "${commit}" != "${GIT_PREVIOUS_SUCCESSFUL_COMMIT}" ]
 then
 echo "Let's check commit ${commit}"
 MESSAGE=$(git log --format=%B -n 1 ${commit})
-echo "Message is: ${MESSAGE}"
+regexp_message ${MESSAGE} string
 fi
 done
 

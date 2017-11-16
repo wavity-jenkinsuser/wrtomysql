04b3c823158cce164e4dc55d8f408d75fd893967
Energy1190 <energyneo0@gmail.com>
2017-11-16 13:43:35 +0300

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -5,11 +5,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'printenv && \
-                echo "${GIT_PREVIOUS_SUCCESSFUL_COMMIT}" && \
-                echo "${GIT_COMMIT}" && \
-                echo "GIT LOG:" && \
-                git rev-list ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}^..HEAD | git log --format=%B -n 1'
+                sh 'remessage.sh'
             }
         }
     }

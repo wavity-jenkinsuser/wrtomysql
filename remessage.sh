c7193f0cccae06a5b8c92f57ecba5cc97fa9f0af
Energy1190 <energyneo0@gmail.com>
2017-11-16 13:49:04 +0300

    Update remessage.sh
    Message text

--- remessage.sh
+++ remessage.sh
@@ -11,7 +11,8 @@ COMMIT_ARRAY=$(git rev-list ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}^..HEAD)
 for commit in "${COMMIT_ARRAY[@]}"
 do
 echo "Let's check commit ${commit}"
-git log --format=%B -n 1 ${commit}
+MESSAGE=$(git log --format=%B -n 1 ${commit})
+echo "Message is: ${MESSAGE}"
 done
 
 echo "Goodbye i'm done."

b08ccba0da44a0b4ae62d5c6c74a955846bce22d
Energy1190 <energyneo0@gmail.com>
2017-11-16 17:44:47 +0300

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -4,7 +4,11 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'chmod +x ${WORKSPACE}/remessage.sh && ${WORKSPACE}/remessage.sh'
+                sh 'echo "Hello, i\'m temp script."'
+                sh 'echo "My var GIT_PREVIOUS_SUCCESSFUL_COMMIT is: ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"'
+                sh 'echo "My var GIT_COMMIT is: ${GIT_COMMIT}"'
+                env.COMMIT_ARRAY= sh 'git rev-list ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}^..HEAD'
+                sh 'echo "Test: ${env.COMMIT_ARRAY[@]}"'
                 mail bcc: 'someone@example.com', body: 'SOME-TEXT', cc: 'someone@example.com', from: '', replyTo: '', subject: 'DETECT ERROR', to: 'someone@example.com'
             }
         }

fc02ac86a3a0928e211dbb0191c1f1f4fe5e0fc4
Energy1190 <energyneo0@gmail.com>
2017-11-16 12:57:37 +0300

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -5,7 +5,9 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'printenv'
+                sh 'printenv 
+                echo "${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
+                echo "${GIT_COMMIT}"'
             }
         }
     }

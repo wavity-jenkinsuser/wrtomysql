f5cd0943fe800c5e068c1d4d39bcd4ce54695da1
Energy1190 <energyneo0@gmail.com>
2017-11-16 13:42:57 +0300

    Update remessage.sh

--- remessage.sh
+++ remessage.sh
@@ -1,1 +1,17 @@
-#!/nin
+#!/usr/bin/env bash
+
+printenv
+
+echo "Hello, i'm temp script."
+
+echo "My var GIT_PREVIOUS_SUCCESSFUL_COMMIT is: ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
+echo "My var GIT_PREVIOUS_SUCCESSFUL_COMMIT is: ${GIT_COMMIT}" 
+echo "My var GIT_PREVIOUS_SUCCESSFUL_COMMIT is: GIT LOG:"
+COMMIT_ARRAY=$(git rev-list ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}^..HEAD)
+for commit in "${COMMIT_ARRAY[@]}"
+do
+echo "Let's check commit ${commit}"
+git log --format=%B -n 1 ${commit}
+done
+
+echo "Goodbye i'm done."

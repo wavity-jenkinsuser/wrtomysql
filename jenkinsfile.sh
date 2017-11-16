6474fb0453232c5df6875ab73a5b84011a5bb170
Energy1190 <energyneo0@gmail.com>
2017-11-16 17:47:32 +0300

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -7,7 +7,9 @@ pipeline {
                 sh 'echo "Hello, i\'m temp script."'
                 sh 'echo "My var GIT_PREVIOUS_SUCCESSFUL_COMMIT is: ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"'
                 sh 'echo "My var GIT_COMMIT is: ${GIT_COMMIT}"'
-                env.COMMIT_ARRAY= sh 'git rev-list ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}^..HEAD'
+                script {
+                    env.COMMIT_ARRAY= sh 'git rev-list ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}^..HEAD'
+                }
                 sh 'echo "Test: ${env.COMMIT_ARRAY[@]}"'
                 mail bcc: 'someone@example.com', body: 'SOME-TEXT', cc: 'someone@example.com', from: '', replyTo: '', subject: 'DETECT ERROR', to: 'someone@example.com'
             }

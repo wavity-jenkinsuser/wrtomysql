de115a27ef77b6347f6b03b8e2ebdd0022154175
Energy1190 <energyneo0@gmail.com>
2017-11-16 17:29:36 +0300

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env groovy
 pipeline {
     agent any
-
+    checkout([$class: 'GitSCM', branches: [[name: '**']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8a5ae250-42f8-482a-ba21-af74658e34c2', url: 'https://github.com/Energy1190/wrtomysql']]])
     stages {
         stage('Build') {
             steps {
                 sh 'chmod +x ${WORKSPACE}/remessage.sh && ${WORKSPACE}/remessage.sh'
+                mail bcc: 'someone@example.com', body: 'SOME-TEXT', cc: 'someone@example.com', from: '', replyTo: '', subject: 'DETECT ERROR', to: 'someone@example.com'
             }
         }
     }

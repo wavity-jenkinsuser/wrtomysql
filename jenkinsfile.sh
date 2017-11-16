4b6d3207bc243c9e822cb464b1208702c46f1bcb
Energy1190 <energyneo0@gmail.com>
2017-11-16 19:37:06 +0300

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -9,16 +9,16 @@ pipeline {
             steps {
                 script {
                     FILENAME = sh(returnStdout: true, script:'git rev-list ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}^..HEAD')
-                    loop_with_preceding_sh(FILENAME)
-                    loop_bad_message()
+                    BAD = loop_with_preceding_sh(FILENAME)
+                    loop_bad_message(BAD)
                 }
             }
         }
     }
 }
 
-def badMessage = [:]
 def loop_with_preceding_sh(list) {
+    def badMessage = [:]
     array = list.split()
     for (int i = 0; i < array.size(); i++) {
         message = sh(returnStdout: true, script: "git log --format=%B -n 1 ${array[i]}")
@@ -30,10 +30,11 @@ def loop_with_preceding_sh(list) {
             badMessage.push(array[i])
         }
     }
+    return badMessage
 }
 
-def loop_with_preceding_sh() {
-    for (int i = 0; i < badMessage.size(); i++) {
-        echo "hi Bad Message badMessage[i]"
+def loop_with_preceding_sh(list) {
+    for (int i = 0; i < list.size(); i++) {
+        echo "hi Bad Message ${list[i]}"
     }
 }

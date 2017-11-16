c6720aa6de761c142a8dc4a51bb7a5b2875f9e6a
Energy1190 <energyneo0@gmail.com>
2017-11-16 17:31:01 +0300

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 pipeline {
     agent any
-    checkout([$class: 'GitSCM', branches: [[name: '**']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8a5ae250-42f8-482a-ba21-af74658e34c2', url: 'https://github.com/Energy1190/wrtomysql']]])
     stages {
         stage('Build') {
             steps {

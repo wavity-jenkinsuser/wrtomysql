9aadf0be9a2e593acdf58eb56bfabfbd63aaef98
Energy1190 <energyneo0@gmail.com>
2017-11-16 12:26:20 +0300

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -5,7 +5,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'git log -n 1 --pretty=format:"%h"'
+                sh 'printenv'
             }
         }
     }

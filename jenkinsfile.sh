fd654e676267e16e84aa4c564674c0b24d60380d
Energy1190 <energyneo0@gmail.com>
2017-11-17 11:58:16 +0300

    finale

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -7,15 +7,12 @@ pipeline {
     stages {
         stage('Test message') {
             steps {
-                sh 'printenv'
                 script {
-                    FILENAME = sh(returnStdout: true, script:'git rev-list ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}^..HEAD')
-                    BADLIST = loop_with_preceding_sh(FILENAME)
+                    COMMITLIST = sh(returnStdout: true, script:'git rev-list ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}^..HEAD')
+                    BADLIST = loop_with_preceding_sh(COMMITLIST)
                     BOOL = loop_bad_message(BADLIST)
                     if (BOOL) {
-                        echo "hi ${BADLIST}"
                         loop_mail_send(BADLIST)
-                        mail bcc: 'energyneo0@gmail.com', body: 'Hello, we have problems: ${BADLIST}', cc: 'energyneo0@gmail.com', from: '', replyTo: '', subject: 'hi', to: 'energyneo0@gmail.com'
                     }
                 }
             }
@@ -51,6 +48,7 @@ def loop_mail_send(list) {
     for (int i = 0; i < list.size(); i++) {
         autor = sh(returnStdout: true, script: "git log --format=%ae -n 1 ${list[i]}")
         message = sh(returnStdout: true, script: "git log --format=%B -n 1 ${list[i]}")
-        echo "Autor: ${autor} and message ${message}"
+        echo 'Hello, ${autor}!  We have problems in commit ${list[i]} with a message ${message}'
+        mail bcc: ${autor}, body: 'Hello, ${autor}! We have problems in commit ${list[i]} with a message ${message}', cc: ${autor}, from: '', replyTo: '', subject: 'hi', to: ${autor}
     }
 }

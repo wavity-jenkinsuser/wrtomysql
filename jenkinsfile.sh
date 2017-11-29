88b24a1b5efaa0a6bf83090e868880e36ed74ce0
Energy1190 <energyneo0@gmail.com>
2017-11-29 12:09:57 +0300

    Update jenkinsfile.sh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -4,15 +4,17 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                def scmCheckout = checkout([$class: 'GitSCM', 
-                                    branches: [[name: 'master']], 
-                                    doGenerateSubmoduleConfigurations: false, 
-                                    extensions: [], 
-                                    submoduleCfg: [], 
-                                    userRemoteConfigs: [[credentialsId: '8a5ae250-42f8-482a-ba21-af74658e34c2', url: 'https://github.com/Energy1190/wrtomysql']]
-                                   ])
-                GPSC = scmCheckout.GIT_PREVIOUS_SUCCESSFUL_COMMIT
-                sh 'echo ${GPSC}'
+                script {
+                    def scmCheckout = checkout([$class: 'GitSCM', 
+                                        branches: [[name: 'master']], 
+                                        doGenerateSubmoduleConfigurations: false, 
+                                        extensions: [], 
+                                        submoduleCfg: [], 
+                                        userRemoteConfigs: [[credentialsId: '8a5ae250-42f8-482a-ba21-af74658e34c2', url: 'https://github.com/Energy1190/wrtomysql']]
+                                       ])
+                    GPSC = scmCheckout.GIT_PREVIOUS_SUCCESSFUL_COMMIT
+                    sh 'echo ${GPSC}'
+                }
             }
         }
         stage('Test message') {
